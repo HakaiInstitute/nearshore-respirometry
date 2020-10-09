aa80aff3f62abc7dc8a0d1063a798d72511129c5
Angeleen <angeleenolson@gmail.com>
2020-10-09 11:13:12 -0700

    started R script to work on

---
+++ calvert_respirometry_wip.R
@@ -0,0 +1,10 @@
+###############################################################################
+# Exploratory Nearshore Respirometry R Script #
+# Code adapted from Nate S, UFloridasee package `respR`                       #
+# Created by AO and OP on Oct 11, 2020 #
+# Automate Respirometry-Related Data. https://github.com/januarharianto/respr,#
+
+
+install.packages("devtools")
+devtools::install_github("januarharianto/respR")
+library(respR)
